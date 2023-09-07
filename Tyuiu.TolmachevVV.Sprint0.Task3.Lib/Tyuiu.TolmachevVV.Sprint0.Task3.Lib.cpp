ed552f5a638af016c7e33ff821d2e3d3c0fe6e0e
Vitaly Tolmachev <vitalhare@outlook.com>
2023-09-07 22:01:16 +0500

    Task2

---
+++ Tyuiu.TolmachevVV.Sprint0.Task3.Lib/Tyuiu.TolmachevVV.Sprint0.Task3.Lib.cpp
@@ -0,0 +1,10 @@
+﻿// Tyuiu.TolmachevVV.Sprint0.Task3.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnTyuiuTolmachevVVSprint0Task3Lib()
+{
+}
