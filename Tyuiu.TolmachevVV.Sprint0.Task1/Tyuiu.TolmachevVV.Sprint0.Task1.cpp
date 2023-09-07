f1742833abcda9ddc039f0155a52c8239a8b9d3f
Vitaly Tolmachev <vitalhare@outlook.com>
2023-09-07 20:07:00 +0500

    Task1

---
+++ Tyuiu.TolmachevVV.Sprint0.Task1/Tyuiu.TolmachevVV.Sprint0.Task1.cpp
@@ -0,0 +1,22 @@
+﻿// Tyuiu.TolmachevVV.Sprint0.Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Zdarova ededede!\n";
+    cout << "Введите ФИО: ";
+    string a;
+    cin >> a;
+    cout << a << endl;
+    int v;
+    cin >> v;
+    cout << "Возраст = " << v;
+    return 0;
+
+}
+
+
